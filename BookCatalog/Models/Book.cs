45e3e2490c3bc5db380c56ab5eb12bfc3cbb6a80
Colt Skeen <colton.skeen@gmail.com>
2022-06-21 16:39:14 -0500

    added scaffold pages for CRUD

--- BookCatalog/Models/Book.cs
+++ BookCatalog/Models/Book.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace BookCatalog.Models
 {
 	public class Book
 	{
-		public Book()
-		{
-		}
+	//PROPERTIES FOR DATABASE
+		public int ID { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+
+		[Range(1, 10, ErrorMessage = "Rating must be between 1 an 10")]
+		public int Rating { get; set; }
 	}
 }
 
